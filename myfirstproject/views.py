4a90b987aa73f80c920a673a98b2b2ee9c4c7870
Mirev <snopkovdmitrii@gmail.com>
2023-08-16 19:26:37 +0600

    Add reverse feature

--- myfirstproject/views.py
+++ myfirstproject/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    print(user_text)
+    reversed_text = user_text[::-1]
+    print(reversed_text)
+    return render(request, 'reverse.html', {'user_text':user_text, "reversed_text":reversed_text})
